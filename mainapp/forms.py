c7fec1d1580bf64b4500193070e021696ca3acc8
omid kazemi <omidkazemi1380@gmail.com>
2023-07-24 15:59:02 +0430

    forms updated

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -1,6 +1,17 @@
+from django import forms
+from .models import UserProfile, Post, Comment
 
-class PostForm():
-    pass
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['bio', 'location', 'birth_date']
 
-class CommentForm():
-    pass
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['text']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['text']
