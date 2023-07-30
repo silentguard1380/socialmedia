ba9699888a1a537a07db10b1763a2006c2855843
Mohammad Zeynali <mhmdzynaly977@gmail.com>
2023-07-30 12:27:46 +0330

    The desktop menu sidebar was developed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./templates/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: "1rem",
+      },
+      fontFamily: {
+        ProductSansRegular: ["ProductSans-Regular"],
+        ProductSansMedium: ["ProductSans-Medium"],
+        ProductSansBold: ["ProductSans-Bold"],
+      },
+      colors: {
+        primary: "rgb(29,155,240)",
+        secondary: "rgb(231,233,234)",
+        hoverBg: "rgba(231,233,234,0.1)",
+      },
+    },
   },
   plugins: [],
 };
