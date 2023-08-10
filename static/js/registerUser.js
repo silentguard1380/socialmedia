2f56922f5153345117c70e79cec3dc577519deb7
Mohammad Zeynali <mhmdzynaly977@gmail.com>
2023-08-10 23:12:07 +0330

    The password display feature was developed on the name and login page

--- static/js/registerUser.js
+++ static/js/registerUser.js
@@ -24,6 +24,7 @@ const years = [];
 const monthSelectBox = document.querySelector("#monthSelectBox");
 const daySelectBox = document.querySelector("#daySelectBox");
 const yearsSelectBox = document.querySelector("#yearsSelectBox");
+const registerFormContainer = document.querySelector("#registerForm");
 const emailInputElem = document.querySelector("#emailInput");
 const passwordInputElem = document.querySelector("#passwordInput");
 const emailValidationMessage = document.querySelector(
@@ -31,6 +32,11 @@ const emailValidationMessage = document.querySelector(
 );
 const passwordValidationMessage = document.querySelector(
   "#passwordValidationMessage"
+);
+const registerButton = document.querySelector("#registerButton");
+const eyePasswordRegister = document.querySelector("#eyePasswordRegister");
+const eyePasswordRegisterIcon = document.querySelector(
+  "#eyePasswordRegisterIcon"
 );
 
 // regex rule
@@ -58,9 +64,11 @@ function InputValidationStatus(
   if (resultValidation) {
     mainInputElem.style.borderColor = "rgb(29,155,240)";
     mainInputValidationMessage.classList.add("hidden");
+    registerButton.classList.remove("mt-14");
   } else {
     mainInputElem.style.borderColor = "rgb(244, 33, 46)";
     mainInputValidationMessage.classList.remove("hidden");
+    registerButton.classList.add("mt-14");
   }
 }
 
@@ -72,6 +80,10 @@ function InputValidationStatus(
   renderOptionElementToDom(years, yearsSelectBox);
 })();
 
+// Set Events
+registerFormContainer.addEventListener("submit", (event) =>
+  event.preventDefault()
+);
 emailInputElem.addEventListener("keyup", () =>
   InputValidationStatus(emailRegex, emailInputElem, emailValidationMessage)
 );
@@ -82,3 +94,19 @@ passwordInputElem.addEventListener("keyup", () =>
     passwordValidationMessage
   )
 );
+eyePasswordRegister.addEventListener("click", () => {
+  if (passwordInputElem.type === "password") {
+    passwordInputElem.type = "text";
+    eyePasswordRegisterIcon.innerHTML = "";
+    eyePasswordRegisterIcon.innerHTML = `
+  <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" />
+`;
+  } else {
+    passwordInputElem.type = "password";
+    eyePasswordRegisterIcon.innerHTML = "";
+    eyePasswordRegisterIcon.innerHTML = `
+<path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+`;
+  }
+});
